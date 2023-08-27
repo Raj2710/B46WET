6afe06600a0440e76a296e9fea8de27db030cc16
Raj2710 <nagarajansai2727@gmail.com>
2023-08-27 13:46:02 +0530

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const UserRoute = require('./routes/users')
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use('/users',UserRoute)
